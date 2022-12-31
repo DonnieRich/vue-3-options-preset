05bc2f469ad15cd06293c3627baef2d21118bcc4
Donnie <donniepask@gmail.com>
2022-12-31 18:04:16 +0100

    New test for writeJsonDataToFile

---
+++ __tests__/checkWriteJsonDataToFileException.test.js
@@ -0,0 +1,26 @@
+const { vol } = require("memfs");
+
+jest.mock("fs/promises");
+
+const { writeJsonDataToFile } = require("../src/bin/writeJsonDataToFile");
+
+describe(writeJsonDataToFile, () => {
+    beforeEach(() => {
+        vol.reset();
+    });
+
+    it("It should throw an error if called with an empty object", async () => {
+
+        const data = {
+        };
+
+        vol.fromJSON(
+            {
+                "package.json": "..."
+            },
+            '/'
+        );
+
+        await expect(() => writeJsonDataToFile("", data)).rejects.toThrow("Empty object provided!");
+    });
+});
