2c9346d0aaa17adcd0187395e82e12e6fd9afaad
Donnie <donniepask@gmail.com>
2022-12-31 18:04:04 +0100

    Refactored writeJsonDataToFile

--- src/bin/writeJsonDataToFile.js
+++ src/bin/writeJsonDataToFile.js
@@ -4,15 +4,12 @@ const writeJsonDataToFile = async (fileBasePath, data) => {
     console.log('\x1b[37m%s\x1b[0m', `📑  Writing to ${fileBasePath}/package.json...`);
     let jsonData = JSON.stringify(data);
 
-    try {
-
-        await fs.writeFile(`${fileBasePath}/package.json`, jsonData, { encoding: 'utf-8' });
-
-    } catch (err) {
-        console.log('\x1b[31m%s\x1b[0m', `❌  Error! Cannot write JSON data to file. Error: ${err}`);
+    if (jsonData === '{}') {
+        throw new Error("Empty object provided!");
     }
 
-    //return jsonData;
+    await fs.writeFile(`${fileBasePath}/package.json`, jsonData, { encoding: 'utf-8' });
+
 }
 
 module.exports = { writeJsonDataToFile };
