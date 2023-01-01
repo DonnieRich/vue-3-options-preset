c3c57e1e2ed26b74b36f9d166810c1e09d3c2a20
Donnie <donniepask@gmail.com>
2023-01-01 11:49:12 +0100

    Removed reference to packageJson object

--- src/bin/mergeJsonObjects.js
+++ src/bin/mergeJsonObjects.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 
 const mergeJsonObjects = async (stubJson, packageJson) => {
     console.log('\x1b[37m%s\x1b[0m', `📑  Merging JSON objects...`);
-    let jsonData = packageJson ?? {};
+    let jsonData = { ...packageJson } ?? {};
 
     try {
         const stubPackageJsonKeys = Object.keys(stubJson);
