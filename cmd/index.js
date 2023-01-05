638e1f709a300d30e3e8b62f2cb5c7fdd5449320
Donnie <donniepask@gmail.com>
2023-01-06 00:24:27 +0100

    Added exitCode

--- cmd/index.js
+++ cmd/index.js
@@ -28,7 +28,8 @@ const init = async () => {
 
         console.log('\x1b[36m%s\x1b[0m', '✅  Your Vue 3 project is now ready! Just run: npm install');
     } catch (err) {
-        console.log('\x1b[31m%s\x1b[0m', `❌  Error! Cannot complete the scaffolding process. Error: ${err}`);
+        console.log('\x1b[31m%s\x1b[0m', `❌  Error! Cannot complete the scaffolding process.`);
+        process.exitCode = 1;
     }
 };
 
