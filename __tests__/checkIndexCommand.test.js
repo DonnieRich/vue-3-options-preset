39ab97990df770b6d7dfc7909cd4af5b2dd8c835
Donnie <donniepask@gmail.com>
2023-01-10 13:04:24 +0100

    Refactored test environment preparation

--- __tests__/checkIndexCommand.test.js
+++ __tests__/checkIndexCommand.test.js
@@ -6,12 +6,12 @@ const stubJson = require('../src/stubs/package.json');
 const baseJson = require('./__jsons__/base.json');
 
 const { BASE_DIR, BASE_STUBS_DIR, JSON_FILE } = config.get();
+const test = prepareEnvironment();
 
 describe('e2e test vue-3-options-preset', () => {
 
-    it("Should pass if the command is run, the scaffolding process is completed and package.json has updated devDependencies", async () => {
-
-        const { makeDir, writeFile, readFile, execute, cleanup, exists } = await prepareEnvironment();
+    beforeEach(async () => {
+        const { writeFile } = await test;
 
         await Promise.allSettled([
             writeFile(`.${JSON_FILE}`, JSON.stringify(baseJson)),
@@ -27,10 +27,19 @@ describe('e2e test vue-3-options-preset', () => {
             writeFile(`${BASE_DIR}/App.vue`, '...'),
             writeFile(`${BASE_DIR}/style.css`, '...'),
             writeFile(`${BASE_DIR}/main.js`, '...'),
-            makeDir(`${BASE_DIR}/components`),
             writeFile(`${BASE_DIR}/components/HelloWorld.vue`, 'Original HelloWorld.vue'),
         ]);
 
+    });
+
+    afterEach(async () => {
+        const { cleanup } = await test;
+        await cleanup();
+    })
+
+    it("Should pass if the command is run, the scaffolding process is completed and package.json has updated devDependencies", async () => {
+
+        const { readFile, execute, exists } = await test;
         const { code, stdout } = await execute('node', './cmd/index.js');
 
         expect(code).toBe(0);
@@ -42,32 +51,11 @@ describe('e2e test vue-3-options-preset', () => {
         expect(await readFile(`${BASE_DIR}/vite.config.js`)).toBe('...');
         expect(await readFile(`.${JSON_FILE}`)).toMatchSnapshot();
 
-        await cleanup();
-
     });
 
     it("Should pass if the command is run, the scaffolding process is completed and package.json has updated dependencies", async () => {
 
-        const { makeDir, writeFile, readFile, execute, cleanup, exists } = await prepareEnvironment();
-
-        await Promise.allSettled([
-            writeFile(`.${JSON_FILE}`, JSON.stringify(baseJson)),
-
-            writeFile(`${BASE_STUBS_DIR}${JSON_FILE}`, JSON.stringify(stubJson)),
-            writeFile(`${BASE_STUBS_DIR}/App.vue`, 'Stub App.vue'),
-            writeFile(`${BASE_STUBS_DIR}/general.scss`, 'SCSS NO BOOTSTRAP'),
-            writeFile(`${BASE_STUBS_DIR}/general-bootstrap.scss`, 'SCSS WITH BOOTSTRAP'),
-            writeFile(`${BASE_STUBS_DIR}/HelloWorld.vue`, 'Stub HelloWorld.vue'),
-            writeFile(`${BASE_STUBS_DIR}/main.js`, 'Stub main.js'),
-
-            writeFile(`${BASE_DIR}/vite.config.js`, '...'),
-            writeFile(`${BASE_DIR}/App.vue`, '...'),
-            writeFile(`${BASE_DIR}/style.css`, '...'),
-            writeFile(`${BASE_DIR}/main.js`, '...'),
-            makeDir(`${BASE_DIR}/components`),
-            writeFile(`${BASE_DIR}/components/HelloWorld.vue`, 'Original HelloWorld.vue'),
-        ]);
-
+        const { readFile, execute, exists } = await test;
         const { code, stdout } = await execute('node', './cmd/index.js -b');
 
         expect(code).toBe(0);
@@ -79,31 +67,16 @@ describe('e2e test vue-3-options-preset', () => {
         expect(await readFile(`${BASE_DIR}/vite.config.js`)).toBe('...');
         expect(await readFile(`.${JSON_FILE}`)).toMatchSnapshot();
 
-        await cleanup();
-
     });
 
     it("Should fail if the command is not run on a fresh installation of Vite and Vue 3", async () => {
 
-        const { makeDir, writeFile, readFile, execute, cleanup, exists } = await prepareEnvironment();
+        const { writeFile, readFile, removeFile, execute, exists } = await test;
 
-        await Promise.allSettled([
-            writeFile(`.${JSON_FILE}`, JSON.stringify(baseJson)),
-
-            writeFile(`${BASE_STUBS_DIR}${JSON_FILE}`, JSON.stringify(stubJson)),
-            writeFile(`${BASE_STUBS_DIR}/App.vue`, 'Stub App.vue'),
-            writeFile(`${BASE_STUBS_DIR}/general.scss`, 'SCSS NO BOOTSTRAP'),
-            writeFile(`${BASE_STUBS_DIR}/general-bootstrap.scss`, 'SCSS WITH BOOTSTRAP'),
-            writeFile(`${BASE_STUBS_DIR}/HelloWorld.vue`, 'Stub HelloWorld.vue'),
-            writeFile(`${BASE_STUBS_DIR}/main.js`, 'Stub main.js'),
-
-            writeFile(`${BASE_DIR}/vite.config.js`, '...'),
-            writeFile(`${BASE_DIR}/App.vue`, '...'),
-            writeFile(`${BASE_DIR}/styles/not-general.scss`, '...'),
-            writeFile(`${BASE_DIR}/main.js`, '...'),
-            makeDir(`${BASE_DIR}/components`),
-            writeFile(`${BASE_DIR}/components/NotHelloWorld.vue`, 'Original NotHelloWorld.vue'),
-        ]);
+        // Scaffolding for this specific test
+        await removeFile(`${BASE_DIR}/style.css`)
+        await writeFile(`${BASE_DIR}/styles/not-general.scss`, '...');
+        await writeFile(`${BASE_DIR}/components/NotHelloWorld.vue`, 'Original NotHelloWorld.vue');
 
         const { code, stdout } = await execute('node', './cmd/index.js');
 
@@ -113,8 +86,7 @@ describe('e2e test vue-3-options-preset', () => {
         expect(await exists(`${BASE_DIR}/styles/general.scss`)).toBe(false);
         expect(await readFile(`${BASE_DIR}/vite.config.js`)).toBe('...');
 
-        await cleanup();
-
     });
 
 });
+
