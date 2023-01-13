483a91d58e5a915f90aeffda1407052011050896
Donnie <donniepask@gmail.com>
2023-01-13 20:20:27 +0100

    Replaced forEach with for-of

--- src/bin/cleanupScaffolding.js
+++ src/bin/cleanupScaffolding.js
@@ -3,11 +3,11 @@ const { removeFile } = require('./removeFile');
 const cleanupScaffolding = async (paths = [], extensions = []) => {
     console.log('\x1b[37m%s\x1b[0m', '♻️  Removing old files...');
 
-    paths.forEach((path) => {
-        extensions.forEach(async (extension) => {
+    for (const path of paths) {
+        for (const extension of extensions) {
             await removeFile(path, extension);
-        });
-    });
+        }
+    }
 };
 
 module.exports = { cleanupScaffolding };
