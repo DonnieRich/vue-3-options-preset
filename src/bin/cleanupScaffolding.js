d272ba545b8413a3aa7da4a8cfe84877ee2b3e49
Donnie <donniepask@gmail.com>
2023-01-16 18:48:10 +0100

    Refactoring for file operations

--- src/bin/cleanupScaffolding.js
+++ src/bin/cleanupScaffolding.js
@@ -1,11 +1,11 @@
-const { removeFile } = require('./removeFile');
+const { fileOperations } = require('./fileOperations');
 
 const cleanupScaffolding = async (paths = [], extensions = []) => {
     console.log('\x1b[37m%s\x1b[0m', '♻️  Removing old files...');
 
     for (const path of paths) {
         for (const extension of extensions) {
-            await removeFile(path, extension);
+            await fileOperations.removeFile(path, extension);
         }
     }
 };
---
+++ src/bin/fileOperations.js
@@ -0,0 +1,3 @@
+const { removeFile } = require("./removeFile");
+
+module.exports = { fileOperations: { removeFile } };
