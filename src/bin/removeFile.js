6b8fe21522e0e655e6fc3fe6b68a8260524b889e
Donnie <donniepask@gmail.com>
2023-01-17 11:29:27 +0100

    Check if dir exists

--- src/bin/removeFile.js
+++ src/bin/removeFile.js
@@ -1,16 +1,22 @@
 const fs = require("fs/promises");
 const path = require("path");
+const { constants } = require("fs");
 
 const { config } = require('../config/config.production');
 const { PROTECTED_FILES } = config.get();
 
 const removeFile = async (baseDir, extension) => {
-    for (const file of await fs.readdir(baseDir)) {
-        if (file.includes(extension) && !PROTECTED_FILES.includes(file)) {
-            await fs.unlink(path.join(baseDir, file));
-            console.log('\x1b[36m%s\x1b[0m', `✅  ${file} removed!`);
-        }
-    }
+    await fs.access(baseDir, constants.F_OK)
+        .then(async () => {
+            for (const file of await fs.readdir(baseDir)) {
+                if (file.includes(extension) && !PROTECTED_FILES.includes(file)) {
+                    await fs.unlink(path.join(baseDir, file));
+                    console.log('\x1b[36m%s\x1b[0m', `✅  ${file} removed!`);
+                }
+            }
+        })
+        .catch((err) => console.log('\x1b[36m%s\x1b[0m', `⚠️  ${baseDir} does not exist...`));
+
 };
 
 module.exports = { removeFile };
