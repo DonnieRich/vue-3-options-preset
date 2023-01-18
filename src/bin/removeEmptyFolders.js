500b693dc942f0dbe6fe1bfcaef9c242019f13e4
Donnie <donniepask@gmail.com>
2023-01-18 16:18:06 +0100

    Added check if dir exist in removeEmptyFolders

--- src/bin/removeEmptyFolders.js
+++ src/bin/removeEmptyFolders.js
@@ -1,14 +1,19 @@
 const fs = require("fs/promises");
+const { constants } = require("fs");
 
 const removeEmptyFolders = async (folders, removableFolders) => {
     for (const folder of folders) {
         if (removableFolders.includes(folder)) {
-            if (await fs.readdir(folder).length === 0) {
-                await fs.rmdir(folder);
-                console.log('\x1b[36m%s\x1b[0m', `✅  ${folder} directory removed!`);
-            } else {
-                console.log('\x1b[36m%s\x1b[0m', `⚠️  ${folder} is not empty...`)
-            }
+            await fs.access(folder, constants.F_OK)
+                .then(async () => {
+                    if (await fs.readdir(folder).length === 0) {
+                        await fs.rmdir(folder);
+                        console.log('\x1b[36m%s\x1b[0m', `✅  ${folder} directory removed!`);
+                    } else {
+                        console.log('\x1b[36m%s\x1b[0m', `❌  ${folder} is not empty...`)
+                    }
+                })
+                .catch((err) => console.log('\x1b[36m%s\x1b[0m', `⚠️  ${folder} does not exist...`));
         }
     }
 };
