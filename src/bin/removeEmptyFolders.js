4ef426f85fae0b397314a8d63561b9ea09a0163c
Donnie <donniepask@gmail.com>
2023-01-22 18:55:54 +0100

    Better log message and zero retry on ENOEMPTY

--- src/bin/removeEmptyFolders.js
+++ src/bin/removeEmptyFolders.js
@@ -2,14 +2,14 @@ const fs = require("fs/promises");
 
 const removeEmptyFolders = async (folder) => {
     return await new Promise((resolve, reject) => {
-        fs.rmdir(folder)
+        fs.rmdir(folder, { maxRetries: 0 })
             .then(async () => {
                 const message = `✅  ${folder} directory removed!`;
                 console.log('\x1b[36m%s\x1b[0m', message);
                 resolve(message);
             })
             .catch((err) => {
-                console.log('\x1b[36m%s\x1b[0m', `⚠️  ${err}`);
+                console.log('\x1b[36m%s\x1b[0m', `⚠️  ${err.message}`);
                 // If the folder is missing, resolve with a message
                 if (err.code === 'ENOENT') {
                     resolve(err.message);
